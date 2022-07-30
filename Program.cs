3790ac7d7e6ec7f974fc5321438173da95801d42
mangosd <mangosd@hotmail.de>
2022-07-30 17:11:17 +0200

    make progressbar fancy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using ShellProgressBar;
 
 var command = "-y -vsync 0 -i \"F:\\Tixati Downloads\\TV Shows\\Vikings.S03.1080p.BluRay.x265-RARBG\\Vikings.S03E01.1080p.BluRay.x265-RARBG.mp4\" -c:a copy -c:v h264_nvenc -pix_fmt yuv420p -b:v 2M -map_metadata 0 Vikings.S03E01.1080p.BluRay.x264.mp4";
 var timeSpan = TimeSpan.FromMinutes(49).Add(TimeSpan.FromSeconds(5));
@@ -10,6 +11,19 @@ p.StartInfo.UseShellExecute = false;
 p.StartInfo.CreateNoWindow = true;
 p.StartInfo.FileName = "ffmpeg";
 p.StartInfo.Arguments = command;
+
+var options = new ProgressBarOptions
+{
+    ProgressCharacter = '█',
+    ProgressBarOnBottom = true,
+    ForegroundColor = ConsoleColor.Green,
+    BackgroundColor = ConsoleColor.DarkGreen,
+    BackgroundCharacter = '░',
+    DisableBottomPercentage = true,
+    PercentageFormat = $"{0:N4}%"
+};
+
+var pbar = new ProgressBar(100, "Encoding", options);
 
 /*p.OutputDataReceived += new DataReceivedEventHandler((s, e) =>
 {
@@ -32,8 +46,15 @@ p.ErrorDataReceived += new DataReceivedEventHandler((s, e) =>
         time = TimeSpan.Parse(timeString);
     }
 
-    Console.WriteLine(e.Data);
-    Console.WriteLine($"{Math.Round((decimal)time.Ticks / (decimal)timeSpan.Ticks, 3) * 100}%");
+    //Console.WriteLine(e.Data);
+    //Console.WriteLine($"{Math.Round((decimal)time.Ticks / (decimal)timeSpan.Ticks, 4) * 100}%");
+
+    var progressPercentDecimal = Math.Round((decimal)time.Ticks / (decimal)timeSpan.Ticks, 4);
+    var progressPercentString = $"{progressPercentDecimal * 100:N2}%";
+    
+    var progress = pbar.AsProgress<double>();
+    progress.Report(Convert.ToDouble(progressPercentDecimal));
+    pbar.Message = $"Progress: {progressPercentString} | Video Progress: {time:hh\\:mm\\:ss} / {timeSpan:hh\\:mm\\:ss}";
 });
 
 p.Start();
